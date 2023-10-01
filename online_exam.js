40e2177eed1ab57aa4263e3d22b03f775fe10bb5
Ramakrishna515 <109713309+Ramakrishna515@users.noreply.github.com>
2023-10-01 17:15:31 +0530

    Add files via upload

---
+++ online_exam.js
@@ -0,0 +1,164 @@
+let timeLeft = document.querySelector(".time-left");
+let quizContainer = document.getElementById("container");
+let nextBtn = document.getElementById("next-button");
+let countOfQuestion = document.querySelector(".number-of-question");
+let displayContainer = document.getElementById("display-container");
+let scoreContainer = document.querySelector(".score-container");
+let restart = document.getElementById("restart");
+let userScore = document.getElementById("user-score");
+let startScreen = document.querySelector(".start-screen");
+let startButton = document.getElementById("start-button");
+let questionCount;
+let scoreCount = 0;
+let count = 11;
+let countdown;
+
+
+
+const quizArray = [{
+        id: "0",
+        question: "What is the full form of CPU?",
+        options: ["Computer Processing Unit", " Computer Principle Unit", "Central Processing Unit", "Control Processing Unit"],
+        correct: "Central Processing Unit",
+    },
+    {
+        id: "1",
+        question: "Which of the following computer language is written in binary codes only?",
+        options: ["pascal", " machine language", "C", "C#"],
+        correct: "machine language",
+    },
+    {
+        id: "2",
+        question: "Who invented Computer?",
+        options: ["Charles Babbage", "Henry Luce", "Henry Babbage", "Charles Luce"],
+        correct: "Charles Babbage",
+    },
+    {
+        id: "3",
+        question: "Which of the following is the brain of the computer?",
+        options: ["Central Processing Unit", "Memory", "Arithmetic and Logic unit", "Control unit"],
+        correct: "Central Processing Unit",
+    },
+    {
+        id: "4",
+        question: "Which of the following is not a characteristic of a computer?",
+        options: ["Versatility", "Accuracy", "Diligence", "I.Q"],
+        correct: "I.Q",
+    },
+
+];
+
+
+restart.addEventListener("click", () => {
+    initial();
+    displayContainer.classList.remove("hide");
+    scoreContainer.classList.add("hide");
+});
+
+
+nextBtn.addEventListener("click",
+    (displayNext = () => {
+        questionCount += 1;
+        if (questionCount === quizArray.length) {
+            displayContainer.classList.add("hide");
+            scoreContainer.classList.remove("hide");
+
+            userScore.innerHTML =
+                "Your score is " + scoreCount + " out of " + questionCount;
+        } else {
+            countOfQuestion.innerHTML =
+                questionCount + 1 + " of " + quizArray.length + " Question";
+            quizDisplay(questionCount);
+            count = 11;
+            clearInterval(countdown);
+            timerDisplay();
+        }
+    })
+);
+
+
+const timerDisplay = () => {
+    countdown = setInterval(() => {
+        count--;
+        timeLeft.innerHTML = `${count}s`;
+        if (count === 0) {
+            clearInterval(countdown);
+            displayNext();
+        }
+    }, 1000);
+};
+
+const quizDisplay = (questionCount) => {
+    let quizCards = document.querySelectorAll(".container-mid");
+    quizCards.forEach((card) => {
+        card.classList.add("hide");
+    });
+    quizCards[questionCount].classList.remove("hide");
+};
+
+function quizCreator() {
+    quizArray.sort(() => Math.random() - 0.5);
+    for (let i of quizArray) {
+        i.options.sort(() => Math.random() - 0.5);
+        let div = document.createElement("div");
+        div.classList.add("container-mid", "hide");
+        countOfQuestion.innerHTML = 1 + " of " + quizArray.length + " Question";
+        let question_DIV = document.createElement("p");
+        question_DIV.classList.add("question");
+        question_DIV.textContent = i.question;
+        div.appendChild(question_DIV);
+        div.innerHTML += `
+        <button class="option-div" onclick="checker(this)">${i.options[0]}</button>
+        <button class="option-div" onclick="checker(this)">${i.options[1]}</button>
+        <button class="option-div" onclick="checker(this)">${i.options[2]}</button>
+        <button class="option-div" onclick="checker(this)">${i.options[3]}</button>`;
+        quizContainer.appendChild(div);
+    }
+}
+
+function checker(userOption) {
+    let userSolution = userOption.innerText;
+    let question =
+        document.getElementsByClassName("container-mid")[questionCount];
+    let options = question.querySelectorAll(".option-div");
+
+    if (userSolution === quizArray[questionCount].correct) {
+        userOption.classList.add("correct");
+        scoreCount++;
+    } else {
+        userOption.classList.add("incorrect");
+        options.forEach((element) => {
+            if (element.innerText === quizArray[questionCount].correct) {
+                element.classList.add("correct");
+            }
+        });
+    }
+
+    clearInterval(countdown);
+    options.forEach((element) => {
+        element.disabled = true;
+    });
+}
+
+function initial() {
+    quizContainer.innerHTML = "";
+    questionCount = 0;
+    scoreCount = 0;
+    count = 11;
+    clearInterval(countdown);
+    timerDisplay();
+    quizCreator();
+    quizDisplay(questionCount);
+}
+
+
+startButton.addEventListener("click", () => {
+    startScreen.classList.add("hide");
+    displayContainer.classList.remove("hide");
+    initial();
+});
+
+window.onload = () => {
+    startScreen.classList.remove("hide");
+    displayContainer.classList.add("hide");
+};
